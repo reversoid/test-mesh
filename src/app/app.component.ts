2617ce8fea4253dfee1240b597abd4487bf7abb5
George <ancient.gekob@gmail.com>
2022-09-01 20:33:19 +0300

    add animation when loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'test-mesh';
-
-  constructor(private modalService: ModalService) {};
-  
-  ngOnInit() {
-    // this.modalService.openModal();
-  }
+export class AppComponent {
+  public isLoading = true;
 }
